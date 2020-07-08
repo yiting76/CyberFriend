c67275556b5f9b8773c4ab34698c4aeab7974c7f
Stud Horse <lalalatim@yahoo.com.tw>
2020-07-09 02:37:36 +0800

    Updates

---
+++ precache-manifest.f31783548a422d3ad99c48224ae37775.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f7e451d48b403b0cb589fe66bdf8af7",
+    "url": "/CyberFriends/index.html"
+  },
+  {
+    "revision": "c016c2724503137269b8",
+    "url": "/CyberFriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "333b8b910e6e12c81c37",
+    "url": "/CyberFriends/static/css/main.f5453dd4.chunk.css"
+  },
+  {
+    "revision": "c016c2724503137269b8",
+    "url": "/CyberFriends/static/js/2.88b08eb1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CyberFriends/static/js/2.88b08eb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "333b8b910e6e12c81c37",
+    "url": "/CyberFriends/static/js/main.8833a993.chunk.js"
+  },
+  {
+    "revision": "f729d4dfc1364d158537",
+    "url": "/CyberFriends/static/js/runtime-main.19006e3c.js"
+  },
+  {
+    "revision": "9deb7d5fae2c035eb8b7d35f62169309",
+    "url": "/CyberFriends/static/media/Cyberpunk.9deb7d5f.ttf"
+  }
+]);
