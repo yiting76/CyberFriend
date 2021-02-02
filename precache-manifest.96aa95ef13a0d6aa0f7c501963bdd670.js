b74b9eeb5df9ba00440666e1ed8748281158bd50
Stud Horse <lalalatim@yahoo.com.tw>
2021-02-02 22:33:13 +0800

    Updates

---
+++ precache-manifest.96aa95ef13a0d6aa0f7c501963bdd670.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63c96041331968d5dade205a57bf84c1",
+    "url": "/CyberFriends/index.html"
+  },
+  {
+    "revision": "c016c2724503137269b8",
+    "url": "/CyberFriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "be7db28bbb76da394b90",
+    "url": "/CyberFriends/static/css/main.f5453dd4.chunk.css"
+  },
+  {
+    "revision": "c016c2724503137269b8",
+    "url": "/CyberFriends/static/js/2.88b08eb1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CyberFriends/static/js/2.88b08eb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be7db28bbb76da394b90",
+    "url": "/CyberFriends/static/js/main.2101f24b.chunk.js"
+  },
+  {
+    "revision": "f729d4dfc1364d158537",
+    "url": "/CyberFriends/static/js/runtime-main.19006e3c.js"
+  },
+  {
+    "revision": "9deb7d5fae2c035eb8b7d35f62169309",
+    "url": "/CyberFriends/static/media/Cyberpunk.9deb7d5f.ttf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/robofriends-PWA/precache-manifest.a7a9de12fc4e7b3b80b08f4ee19415d0.js"
+  "/CyberFriends/precache-manifest.96aa95ef13a0d6aa0f7c501963bdd670.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/robofriends-PWA/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/CyberFriends/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
